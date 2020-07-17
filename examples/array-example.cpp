668395979f91feac334a4958d169ce7452e00b67
udv-code <dv4code@gmail.com>
2020-07-17 16:28:41 +0300

    Started implementing array.

--- examples/array-example.cpp
+++ examples/array-example.cpp
@@ -1,7 +1,23 @@
 // Copyright (c) 2020 udv. All rights reserved.
 
+#include <iostream>
+
 #include <ustl/array.hpp>
 
-int main (int argc, char** argv) {
-    return 0;
+int main(int argc, char **argv) {
+	ustd::array<int, 10> collection{};
+
+	// for (auto i : collection) {
+	// 	std::cout << i << "\n";
+	// }
+	//
+	// for (auto& i : collection) {
+    //     i = 1;
+	// }
+
+	for (size_t i = 0; i < collection.size(); ++i) {
+		std::cout << collection[i] << "\n";
+	}
+
+	return 0;
 }
--- ustl-main/include/ustl/array.hpp
+++ ustl-main/include/ustl/array.hpp
@@ -4,7 +4,25 @@
 #define USTL_ARRAY
 
 namespace ustd {
+	template<typename T, size_t s>
+	class array {
+		using size_type = size_t;
+	public:
+		[[nodiscard]] constexpr size_type size() const noexcept { return s; }
 
+		// TODO: indexofbounds check in debug
+		[[nodiscard]] T &operator[](size_type index) noexcept { return data_[index]; }
+
+		[[nodiscard]] const T &operator[](size_type index) const noexcept { return data_[index]; }
+
+		[[nodiscard]] const T *data() const noexcept { return data_; }
+		[[nodiscard]] T *data() noexcept { return data_; }
+
+		// TODO: iterators
+
+	private:
+		T data_[s];
+	};
 }
 
 #endif //USTL_ARRAY
